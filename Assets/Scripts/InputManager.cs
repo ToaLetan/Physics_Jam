6f75e2ec4fe59e0451cd35d61d5a5e1ddf64644c
Tyler Norman <letan881@hotmail.com>
2014-05-11 21:49:11 -0400

    Two Player Keyboard Input
    Removed XInput, implemented basic keyboard input for now. Player 1
    hardcoded to use WASD, Player 2 hardcoded to use IJKL.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
-using XInputDotNetPure;
+
+public struct Keybinds
+{
+	public KeyCode UpKey;
+	public KeyCode DownKey;
+	public KeyCode LeftKey;
+	public KeyCode RightKey;
+	public KeyCode LTurnKey;
+	public KeyCode RTurnKey;
+}
 
 public class InputManager
 {
-	public delegate void ControllerButtonEvent(ButtonState buttonState);
+	public delegate void KeyHeldEvent(int playerNum, KeyCode key);
+	public delegate void KeyReleasedEvent(int playerNum, KeyCode key);
 
+	public event KeyHeldEvent Key_Pressed;
+	public event KeyReleasedEvent Key_Released;
+
+	public Keybinds[] PlayerKeybindArray = new Keybinds[2];
+	
 	private static InputManager instance = null;
-
-	public GamePadState[] GamePadStateArray = new GamePadState[4];
-	public GamePadState[] PrevGamePadStateArray = new GamePadState[4];
 
 	public static InputManager Instance
 	{
@@ -24,35 +36,97 @@ public class InputManager
 	// Use this for initialization
 	private InputManager()
 	{
-		for (int i = 0; i < GamePadStateArray.Length; i++) 
-		{
-			GamePadStateArray[i] = GamePad.GetState((PlayerIndex) i);
-			Debug.Log( "Controller " + (i+1) + " connected: " + GamePadStateArray[i].IsConnected);
-		}
+		//Hard-setting Keybinds for players 1 and 2 here for now. Eventually implement options.
+		PlayerKeybindArray [0].UpKey = KeyCode.W;
+		PlayerKeybindArray [0].DownKey = KeyCode.S;
+		PlayerKeybindArray [0].LeftKey = KeyCode.A;
+		PlayerKeybindArray [0].RightKey = KeyCode.D;
+		PlayerKeybindArray [0].LTurnKey = KeyCode.Q;
+		PlayerKeybindArray [0].RTurnKey = KeyCode.E;
+
+		PlayerKeybindArray [1].UpKey = KeyCode.I;
+		PlayerKeybindArray [1].DownKey = KeyCode.K;
+		PlayerKeybindArray [1].LeftKey = KeyCode.J;
+		PlayerKeybindArray [1].RightKey = KeyCode.L;
+		PlayerKeybindArray [1].LTurnKey = KeyCode.U;
+		PlayerKeybindArray [1].RTurnKey = KeyCode.O;
 	}
 	
 	// Update is called once per frame
 	public void Update () 
 	{
-		UpdateControllerStates();
+		UpdateButtonInput ();
 	}
 
-	private void UpdateControllerStates()
-	{
-		//Check if each controller is connected, set the previous state to the current state and update the current state.
-		for (int i = 0; i < GamePadStateArray.Length; i++) 
-		{
-			if(GamePad.GetState((PlayerIndex) i).IsConnected)
-			{
-				PrevGamePadStateArray[i] = GamePadStateArray[i];
-				GamePadStateArray[i] = GamePad.GetState((PlayerIndex) i);
-			}
-			//If the controller is disconnected, maybe pause the game and prompt a reconnect?
-		}
-	}
 
 	private void UpdateButtonInput()
 	{
+		for (int i = 0; i < PlayerKeybindArray.Length; i++) 
+		{
+			//Check all keys being held.
+			if(Input.GetKey(PlayerKeybindArray [i].UpKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].UpKey);
+			}
+			if(Input.GetKey(PlayerKeybindArray [i].DownKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].DownKey);
+			}
+			if(Input.GetKey(PlayerKeybindArray [i].LeftKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].LeftKey);
+			}
+			if(Input.GetKey(PlayerKeybindArray [i].RightKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].RightKey);
+			}
+			if(Input.GetKey(PlayerKeybindArray [i].LTurnKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].LTurnKey);
+			}
+			if(Input.GetKey(PlayerKeybindArray [i].RTurnKey))
+			{
+				if(Key_Pressed != null)
+					Key_Pressed(i, PlayerKeybindArray [i].RTurnKey);
+			}
 
+			//Check all keys being released.
+			if(!Input.GetKey(PlayerKeybindArray [i].UpKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].UpKey);
+			}
+			if(!Input.GetKey(PlayerKeybindArray [i].DownKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].DownKey);
+			}
+			if(!Input.GetKey(PlayerKeybindArray [i].LeftKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].LeftKey);
+			}
+			if(!Input.GetKey(PlayerKeybindArray [i].RightKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].RightKey);
+			}
+			if(!Input.GetKey(PlayerKeybindArray [i].LTurnKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].LTurnKey);
+			}
+			if(!Input.GetKey(PlayerKeybindArray [i].RTurnKey))
+			{
+				if(Key_Released != null)
+					Key_Released(i, PlayerKeybindArray [i].RTurnKey);
+			}
+
+		}
 	}
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,25 +3,44 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour 
 {
+	public int PlayerNumber = 0;
+
+	InputManager inputManager = InputManager.Instance;
+
 	// Use this for initialization
 	void Start () 
 	{
-		InputManager bleh = InputManager.Instance;
-		//assignedController = new XboxController(0);
+		inputManager.Key_Pressed += PlayerInput;
+		inputManager.Key_Released += ApplyDeceleration;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		InputManager.Instance.Update ();
 
-		//float thumbstickX = InputManager.Instance.ControllerArray[0].GetAxis("Horizontal");
-		// thumbstickY = InputManager.Instance.ControllerArray[0].GetAxis("Vertical");
+	}
 
-		//float angle = Mathf.Rad2Deg * Mathf.Atan2 (thumbstickX, thumbstickY);
-		//Vector3 newRotation = new Vector3 (gameObject.transform.rotation.x, gameObject.transform.rotation.y, angle);
+	private void PlayerInput(int playerNum, KeyCode key)
+	{
+		Vector3 newPosition = gameObject.transform.position;
 
+		if (playerNum == PlayerNumber) 
+		{
+			if (key == inputManager.PlayerKeybindArray [playerNum].UpKey)
+					newPosition.y += 0.5f * Time.deltaTime;
+			if (key == inputManager.PlayerKeybindArray [playerNum].DownKey)
+					newPosition.y -= 0.5f * Time.deltaTime;
+			if (key == inputManager.PlayerKeybindArray [playerNum].LeftKey)
+					newPosition.x -= 0.5f * Time.deltaTime;
+			if (key == inputManager.PlayerKeybindArray [playerNum].RightKey)
+					newPosition.x += 0.5f * Time.deltaTime;
+		}
 
-		//gameObject.transform.rotation = Quaternion.Lerp (gameObject.transform.rotation, Quaternion.Euler(newRotation), Time.deltaTime);
+		gameObject.transform.position = newPosition;
+	}
+
+	private void ApplyDeceleration(int PlayerNum, KeyCode key)
+	{
+
 	}
 }
