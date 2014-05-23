02cd5495d33c5b0d388ad964772a6ec13f8b27fc
Tyler Norman <letan881@hotmail.com>
2014-05-22 22:00:35 -0400

    Camera Effects Test
    Created basic camera effects script, currently running it to test. Will
    eventually be used for polish later in development.

---
+++ Assets/Scripts/CameraEffectsScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEffectsScript : MonoBehaviour 
+{
+	private float moveSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RotateToAngle (275, moveSpeed);
+		ZoomLevel (0.8f, moveSpeed);
+	}
+
+	public void RotateToAngle(float angleInDegrees, float rotateSpeed) //Rotate camera towards a specified angle on the Z-axis
+	{
+		Quaternion newRotation = Quaternion.AngleAxis (angleInDegrees, Vector3.forward);
+
+		gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+	}
+
+	public void ZoomLevel(float zoomScale, float zoomSpeed) //Zoom in/out (positive
+	{
+		Camera theCamera = gameObject.GetComponent<Camera>();
+
+		if (theCamera.orthographicSize != zoomScale)
+			theCamera.orthographicSize += (zoomScale - theCamera.orthographicSize) * Time.deltaTime * zoomSpeed;
+
+		//theCamera.orthographicSize = zoomScale;
+	}
+}
